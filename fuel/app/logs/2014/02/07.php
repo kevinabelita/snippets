dcddb2ea866692468c0dd606ba45d6c3c76f8feb
kevinabelita <kevinabelita@gmail.com>
2014-02-18 16:22:01 +0800

    - changed gitignore

--- fuel/app/logs/2014/02/07.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2014-02-07 09:48:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
